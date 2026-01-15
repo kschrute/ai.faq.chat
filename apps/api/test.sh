9f7915205a0cd63bd8a69a9bd51b4f3f72cfd04d
K S <konstantin.shkut@strongblock.io>
2026-01-15 12:29:09 +0000

    cleanup

--- apps/api/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:8000/chat -H "Content-Type: application/json" -d '{"model": "faq-chat", "messages": [{"role": "user", "content": "How do I reset my password?"}]}'
-
-curl -X POST http://localhost:8000/chat -H "Content-Type: application/json" -d '{"model": "faq-chat", "messages": [{"role": "user", "content": "What is quantum computing?"}]}'
